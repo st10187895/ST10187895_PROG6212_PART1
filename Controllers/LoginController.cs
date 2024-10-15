3da6d9926c9deac89182589f4df2890edfe7c560
Thando Fredericks <st10187895@vcconnect.edu.za>
2024-10-16 01:53:06 +0200

    Added Login Controller and  login Model for user validation.
    Set Up connection strings to connect to database.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ST10187895_PROG6212_PART1.Models;
+using System.Diagnostics;
+
+namespace ST10187895_PROG6212_PART1.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly LoginModel Login;
+
+        public LoginController()
+        {
+            Login = new LoginModel();
+        }
+
+        [HttpPost]
+        public ActionResult ValidateUser(string personID, string password)
+        {
+            var loginModel = new LoginModel();
+            string userID = loginModel.findUser(personID, password);
+            if (userID != "-1")
+            {
+                // Store userID in session
+                HttpContext.Session.SetString("UserID", userID);
+
+                //HttpContext.Session.SetInt32("UserID", userID);
+
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // User not found, handle accordingly (e.g., show error message)
+                return View("LoginFailed");
+            }
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace ST10187895_PROG6212_PART1.Models
+{
+    public class LoginModel
+    {
+        public static string con_string = "Server=tcp:st10187895.database.windows.net,1433;Initial Catalog=ST10187895_PROG6212_POE;Persist Security Info=False;User ID=st10187895;Password=MMcCord23$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public string findUser(string personID, string password)
+        {
+            string userId = "-1";
+            using (SqlConnection connection = new SqlConnection(con_string))
+            {
+                string sql = "SELECT contractorID FROM IndependantContractor WHERE contractorID = @personID AND password = @password";
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                cmd.Parameters.AddWithValue("@personID", personID);
+                cmd.Parameters.AddWithValue("@password", password);
+                try
+                {
+                    connection.Open();
+                    object result = cmd.ExecuteScalar();
+                    if (result != null && result != DBNull.Value)
+                    {
+                        userId = (string)result;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return userId;
+        }
+    }
+}
