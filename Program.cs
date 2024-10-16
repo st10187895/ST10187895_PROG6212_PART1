e77afbe3463fca664cccf9410fb6c6d0fe590a7c
Thando Fredericks <st10187895@vcconnect.edu.za>
2024-10-16 15:05:50 +0200

    Working Contractor Login with changes made To Files in Commit with Message 'W' (Accidently clicked commit before completing commit message)

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace ST10187895_PROG6212_PART1
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
